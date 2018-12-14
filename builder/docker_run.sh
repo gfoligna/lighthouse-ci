bae5a4cfb5b3f5422513d0c1d6cf92ec62318d65
Gonzalo Foligna <gfoligna@gmail.com>
2018-12-14 17:19:00 -0300

    Adding mountpoint for reports

--- builder/docker_run.sh
+++ builder/docker_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker kill lighthouse_ci
-docker run -dit -p 8080:8080 --rm --name lighthouse_ci --cap-add=SYS_ADMIN lighthouse_ci
+docker run -dit -p 8080:8080 -v "/home/developer/reports:/home/chrome/reports:rw" --rm --name lighthouse_ci --cap-add=SYS_ADMIN lighthouse_ci
