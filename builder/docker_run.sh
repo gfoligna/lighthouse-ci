3d03df4b811a472c987e98670bc3e6703c7e8f93
Gonzalo Foligna <gfoligna@gmail.com>
2019-01-11 16:48:51 -0300

    Small tweaks

--- builder/docker_run.sh
+++ builder/docker_run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker kill lighthouse_ci
-docker run -dit -p 8080:8080 -p 9222:9222 -v "/home/developer/reports:/home/chrome/reports:rw" --rm --name lighthouse_ci --cap-add=SYS_ADMIN lighthouse_ci
+docker stop lighthouse_ci
+docker rm lighthouse_ci
+docker run -dit -p 8080:8080 -p 9222:9222 -v "/home/developer/reports:/home/chrome/reports:rw" --name lighthouse_ci --cap-add=SYS_ADMIN --restart always --health-cmd "curl --fail http://localhost:9222/ || exit 1" --health-interval=5s --health-timeout=3s lighthouse_ci
