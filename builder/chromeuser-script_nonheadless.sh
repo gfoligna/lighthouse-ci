fca7af42f49771dd22b8d65d1abe818f795e5361
Gonzalo Foligna <gfoligna@gmail.com>
2019-01-11 14:36:11 -0300

    Adding 0.0.0.0 as debugging address for chrome in order to monitor the 'service'

--- builder/chromeuser-script_nonheadless.sh
+++ builder/chromeuser-script_nonheadless.sh
@@ -14,4 +14,5 @@ nohup google-chrome \
   --safebrowsing-disable-auto-update \
   --disable-setuid-sandbox \
   --user-data-dir=${TMP_PROFILE_DIR} \
-  --remote-debugging-port=9222 'about:blank' &
+  --remote-debugging-port=9222 \
+  --remote-debugging-address=0.0.0.0 'about:blank' &
