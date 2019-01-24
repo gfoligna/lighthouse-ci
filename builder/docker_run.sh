3a94efbcd67dca424020dd111225ef42960ac6f8
Gonzalo Foligna <gfoligna@gmail.com>
2019-01-24 15:40:37 -0300

    Changing run method

--- builder/docker_run.sh
+++ builder/docker_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker stop lighthouse_ci
-docker rm lighthouse_ci
+docker kill lighthouse_ci
+docker rm -f lighthouse_ci
 docker run -dit -p 8080:8080 -p 9222:9222 -v "/home/developer/reports:/home/chrome/reports:rw" --name lighthouse_ci --cap-add=SYS_ADMIN --restart always --health-cmd "curl --fail http://localhost:9222/ || exit 1" --health-interval=5s --health-timeout=3s lighthouse_ci
